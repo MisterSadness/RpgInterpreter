853e1c19998170ee06544a59546017ae9f090681
Piotr Lewandowski <p.lewandowski2@student.mini.pw.edu.pl>
2021-11-22 01:24:29 +0100

    Word tests

---
+++ RpgInterpreterTests/LexerTests/LowercaseWordLexerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RpgInterpreter.Lexer;
+using RpgInterpreter.Lexer.InnerLexers;
+using RpgInterpreter.Lexer.Sources;
+using RpgInterpreter.Tokens;
+
+namespace RpgInterpreterTests.LexerTests
+{
+    class LowercaseWordLexerTests
+    {
+        private readonly LowercaseWordLexer _lexer = new();
+
+        [TestCaseSource(nameof(_lowercaseWords))]
+        public void LowercaseWordTest(SingleTestData data)
+        {
+            var result = _lexer.Match(data.Source);
+
+            Assert.That(result, Is.EqualTo(data.Output));
+        }
+
+        private static SingleTestData[] _lowercaseWords = {
+            new ("aA321A1", new LowercaseIdentifier("aA321A1")),
+            new ("witch", new LowercaseIdentifier("witch")),
+            new ("aaaa_aaAADA1321", new LowercaseIdentifier("aaaa_aaAADA1321")),
+            new ("if", new If()),
+            new ("then", new Then()),
+            new ("else", new Else()),
+            new ("trait", new Trait()),
+            new ("for", new For()),
+            new ("with", new With()),
+            new ("extends", new Extends()),
+            new ("and", new And()),
+            new ("base", new Base()),
+            new ("this", new This()),
+        };
+    }
+}
---
+++ RpgInterpreterTests/LexerTests/UppercaseWordLexerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RpgInterpreter.Lexer;
+using RpgInterpreter.Lexer.InnerLexers;
+using RpgInterpreter.Lexer.Sources;
+using RpgInterpreter.Tokens;
+
+namespace RpgInterpreterTests.LexerTests
+{
+    class UppercaseWordLexerTests
+    {
+        private readonly UppercaseWordLexer _lexer = new();
+
+        [TestCaseSource(nameof(_uppercaseWords))]
+        public void UppercaseWordTest(SingleTestData data)
+        {
+            var result = _lexer.Match(data.Source);
+
+            Assert.That(result, Is.EqualTo(data.Output));
+        }
+
+        private static SingleTestData[] _uppercaseWords = {
+            new ("Aaaaaaa", new UppercaseIdentifier("Aaaaaaa")),
+            new ("Jan_Kowalski_99", new UppercaseIdentifier("Jan_Kowalski_99")),
+            new ("A11A1Aaa1_", new UppercaseIdentifier("A11A1Aaa1_"))
+        };
+    }
+}
