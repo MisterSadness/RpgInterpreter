7a56f6bed9364c32aeec7c8de8720d1a3c0ba337
Piotr Lewandowski <p.lewandowski2@student.mini.pw.edu.pl>
2021-12-13 05:03:24 +0100

    actual ll1 grammar

---
+++ RpgInterpreter/Parser/Parser.cs
@@ -0,0 +1,114 @@
+﻿using Optional;
+using Optional.Collections;
+using RpgInterpreter.Lexer;
+using RpgInterpreter.Lexer.Sources;
+using RpgInterpreter.NonTerminals;
+using RpgInterpreter.Productions;
+using RpgInterpreter.Tokens;
+
+namespace RpgInterpreter.Parser;
+
+public class Parser
+{
+    private readonly TokenSource _tokenSource;
+    private readonly ParsingTable _parsingTable;
+    private readonly Stack<Symbol> _stack;
+
+    public Parser(TokenSource tokenSource, ParsingTable parsingTable, Symbol startSymbol)
+    {
+        _tokenSource = tokenSource;
+        _parsingTable = parsingTable;
+        _stack = new Stack<Symbol>();
+        _stack.Push(new Terminal<EndOfInput>());
+        _stack.Push(startSymbol);
+    }
+
+    public virtual void Parse()
+    {
+        ParsingResult state = new Success();
+        while (_stack.Any() && state is not Failure)
+        {
+            var top = _stack.Pop();
+            var input = _tokenSource.Peek().Value;
+            var inputType = input.GetType();
+            state = (top, input) switch
+            {
+                (Terminal<EndOfInput>, EndOfInput) => new Success(),
+                (NonTerminal nt, { }) => HandleNonTerminals(nt, inputType),
+                (Terminal t, { }) => HandleTerminals(t, input),
+                _ => new Failure()
+            };
+        }
+    }
+
+    private ParsingResult HandleNonTerminals(NonTerminal left, Type right)
+    {
+        var production = _parsingTable.Find(left, right);
+        _stack.Pop();
+
+        return new Success();
+    }
+
+    private ParsingResult HandleTerminals(Terminal left, Token right)
+    {
+        if (left.TokenType != right.GetType())
+        {
+            return new Failure();
+        }
+
+        _stack.Pop();
+        _tokenSource.Pop();
+        return new Success();
+    }
+}
+
+public record ParsingResult;
+
+public record Success : ParsingResult;
+
+public record Failure : ParsingResult;
+
+public class ParsingException : Exception { }
+
+public class ParsingTable
+{
+    private readonly Dictionary<(NonTerminal, Type), Production> _table;
+    public ParsingTable(Dictionary<(NonTerminal, Type), Production> table) => _table = table;
+
+    public Option<Production> Find(NonTerminal nonTerminal, Type nextInputType) =>
+        _table.GetValueOrNone((nonTerminal, nextInputType));
+}
+
+public record Symbol;
+
+public abstract record Terminal : Symbol
+{
+    public abstract Type TokenType { get; }
+}
+
+public record Epsilon : Terminal<Whitespace>;
+
+public record Terminal<T> : Terminal where T : Token
+{
+    public override Type TokenType { get; } = typeof(T);
+}
+
+public class TokenSource
+{
+    private readonly IEnumerator<PositionedToken> _tokens;
+
+    public TokenSource(IEnumerable<PositionedToken> tokens) =>
+        _tokens = tokens.GetEnumerator();
+
+    public static TokenSource FromCharSource(ICharSource source) =>
+        new(new TrackingRpgLexer().Tokenize(source));
+
+    public PositionedToken Peek() => _tokens.Current;
+
+    public PositionedToken Pop()
+    {
+        _tokens.MoveNext();
+        return _tokens.Current;
+    }
+}
+
---
+++ RpgInterpreterTests/ParserTests/ParsingTableTests.cs
@@ -0,0 +1,112 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+using RpgInterpreter.NonTerminals;
+using RpgInterpreter.Parser;
+using RpgInterpreter.Productions;
+using RpgInterpreter.Tokens;
+
+namespace RpgInterpreterTests.ParserTests;
+
+internal class ParsingTableTests
+{
+    [TestCase]
+    public void Test1()
+    {
+        var productionList = GetInstancesOfAllTypesInheriting<Production>();
+
+        var table = new ParsingTableGenerator(productionList).CalculateParsingTable();
+
+        Assert.Pass();
+    }
+
+    private IEnumerable<T> GetInstancesOfAllTypesInheriting<T>()
+    {
+        var types = typeof(T).Assembly.GetTypes().Where(t =>
+            t.IsAssignableTo(typeof(T)) && !t.IsAbstract && !t.ContainsGenericParameters);
+        var ctors = types.Select(t =>
+            t.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+                Array.Empty<Type>()));
+
+        return ctors.Select(c => (T)c.Invoke(null));
+    }
+
+    [TestCase]
+    public void Test2()
+    {
+        var productionList = new List<Production>
+        {
+            new SF(), new SParen(), new FA()
+        };
+        var firsts = new ParsingTableGenerator(productionList).CalculateFirst();
+
+        Assert.That(firsts, Is.Not.Empty);
+    }
+
+    [TestCase]
+    public void Test3()
+    {
+        var productionList = new List<Production>
+        {
+            new Start(), new TermExpr1(), new Plus(), new MinusEx(), new ExprEps(), new FactorTerm1(), new Mul(),
+            new Div(),
+            new TermEps(),
+            new Parens(), new Number()
+        };
+        var firsts = new ParsingTableGenerator(productionList).CalculateFirst();
+
+        Assert.That(firsts, Is.Not.Empty);
+    }
+
+    private record S : NonTerminal;
+
+    private record F : NonTerminal;
+
+    private record A : Terminal<StringLiteral>;
+
+    private record SF() : Production<S>(new Symbol[] { new F() });
+
+    private record SParen() : Production<S>(new Symbol[]
+    {
+        new Terminal<OpenParen>(),
+        new S(),
+        new Terminal<Addition>(),
+        new F(),
+        new Terminal<CloseParen>()
+    });
+
+    private record FA() : Production<F>(new Symbol[] { new A() });
+
+    private record Expr : NonTerminal;
+
+    private record Expr1 : NonTerminal;
+
+    private record Term : NonTerminal;
+
+    private record Term1 : NonTerminal;
+
+    private record Factor : NonTerminal;
+
+    private record Start() : Production<S>(new Symbol[] { new Expr(), new Terminal<EndOfInput>() });
+
+    private record TermExpr1() : Production<Expr>(new Symbol[] { new Term(), new Expr1() });
+
+    private record Plus() : Production<Expr1>(new Symbol[] { new Terminal<Addition>(), new Term(), new Expr1() });
+
+    private record MinusEx() : Production<Expr1>(new Symbol[] { new Terminal<Minus>(), new Term(), new Expr1() });
+
+    private record ExprEps() : Production<Expr1>(new Symbol[] { new Epsilon() });
+
+    private record FactorTerm1() : Production<Term>(new Symbol[] { new Factor(), new Term1() });
+
+    private record Mul() : Production<Term1>(new Symbol[]
+        { new Terminal<Multiplication>(), new Factor(), new Term1() });
+
+    private record Div() : Production<Term1>(new Symbol[] { new Terminal<Division>(), new Factor(), new Term1() });
+
+    private record TermEps() : Production<Term1>(new Symbol[] { new Epsilon() });
+
+    private record Parens() : Production<Factor>(new Symbol[]
+        { new Terminal<OpenParen>(), new Expr(), new Terminal<CloseParen>() });
+
+    private record Number() : Production<Factor>(new Symbol[] { new Terminal<NaturalLiteral>() });
+}
