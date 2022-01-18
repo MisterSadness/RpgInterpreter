a1a177e5926f3bf7321fe14a8b42431d17d6ad6e
Piotr Lewandowski <p.lewandowski2@student.mini.pw.edu.pl>
2022-01-18 03:09:33 +0100

    Refactored main

--- RpgInterpreter/Program.cs
+++ RpgInterpreter/Program.cs
@@ -1,39 +1,23 @@
-﻿using RpgInterpreter.Lexer;
+﻿using RpgInterpreter.ExceptionHandler;
+using RpgInterpreter.Lexer;
 using RpgInterpreter.Lexer.Sources;
+using RpgInterpreter.Parser;
 
 if (args.Length == 1 && File.Exists(args[0]))
 {
     var lexer = new TrackingRpgLexer();
     using var source = new FileSource(args[0]);
 
-    //try
-    //{
-    //var tokens = lexer.Tokenize(source).ToArray();
-    //Console.WriteLine($"Successfully lexed, result token total: {tokens.Length}");
-    //Console.WriteLine("Result tokens (without whitespace):");
-    //foreach (var token in tokens.Select(t => t.Value).Where(t => t is not Whitespace)) Console.WriteLine(token);
+    var parser = new CoolerParser(lexer.Tokenize(source));
 
-    //     var parser = new Parser(source);
-    //     var parsingResult = parser.Parse();
-    //     Console.WriteLine(parsingResult);
-    // }
-    // catch (LexingException e)
-    // {
-    //     Console.WriteLine($"Lexing failed with: {e}");
-    // }
-    // catch (ObsoleteUnexpectedTokenException e)
-    // {
-    //     Console.WriteLine(e.Message);
-    // }
-    // catch (ObsoleteExpectedTokenNotFoundException e)
-    // {
-    //     Console.WriteLine(e.Message);
-    // }
-    // catch (Exception)
-    // {
-    //     Console.WriteLine("Unexpected exception has occurred.");
-    //     throw;
-    // }
+    var handler = new RpgInterpreterExceptionHandler();
+
+    handler.RunAndHandle(() =>
+    {
+        var root = parser.Parse();
+
+        Console.WriteLine(root.ToString());
+    });
 }
 else
 {
